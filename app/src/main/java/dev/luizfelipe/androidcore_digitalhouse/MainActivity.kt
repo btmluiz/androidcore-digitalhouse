e01dfb22c613d1ed0a54db735e9bb830c5bef76b
Luiz Felipe Silva Braga <btmluiz@outlook.com>
2020-11-10 14:23:16 -0300

    first commit

---
+++ app/src/main/java/dev/luizfelipe/androidcore_digitalhouse/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.luizfelipe.androidcore_digitalhouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
