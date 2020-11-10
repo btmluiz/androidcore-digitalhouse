a909981dae23304a8a53ee3a9f6b4c2a3bba66b7
Luiz Felipe Silva Braga <btmluiz@outlook.com>
2020-11-10 15:13:00 -0300

    Implementação SplashScreen

---
+++ app/src/main/java/dev/luizfelipe/androidcore_digitalhouse/SplashScreen.kt
@@ -0,0 +1,11 @@
+package dev.luizfelipe.androidcore_digitalhouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
---
+++ app/src/main/java/dev/luizfelipe/androidcore_digitalhouse/ui/login/LoginActivity.kt
@@ -0,0 +1,26 @@
+package dev.luizfelipe.androidcore_digitalhouse.ui.login
+
+import android.app.Activity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+
+import dev.luizfelipe.androidcore_digitalhouse.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
