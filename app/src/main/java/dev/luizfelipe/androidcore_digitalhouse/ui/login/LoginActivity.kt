328f757294db3d73ccf8d06739e994e62e6a0643
Luiz Felipe Silva Braga <btmluiz@outlook.com>
2020-11-13 13:27:23 -0300

    Implementação tela de login

--- app/src/main/java/dev/luizfelipe/androidcore_digitalhouse/ui/login/LoginActivity.kt
+++ app/src/main/java/dev/luizfelipe/androidcore_digitalhouse/ui/login/LoginActivity.kt
@@ -1,6 +1,7 @@
 package dev.luizfelipe.androidcore_digitalhouse.ui.login
 
 import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -14,13 +15,26 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import dev.luizfelipe.androidcore_digitalhouse.MainActivity
 
 import dev.luizfelipe.androidcore_digitalhouse.R
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var loginBtn: Button
+    private lateinit var registerBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginBtn = findViewById(R.id.login)
+        registerBtn = findViewById(R.id.registerBtn)
+
+        loginBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        }
     }
 }
